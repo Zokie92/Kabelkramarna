d147a11c57646dd32b333d80b24125301b21de53
Nickzore <nicke_fr@hotmail.com>
2025-10-23 22:30:32 +0200

    Update Niclas_F.py

--- Niclas_F.py
+++ Niclas_F.py
@@ -1,36 +1,36 @@
-import socket
+import socket #importera socket från bibiloteket för nätverksanslutningar.
 
-host = "scanme.nmap.org"
-timeout_seconds = 2.0
+host = "scanme.nmap.org" # hostserverns namn.
+timeout_seconds = 2.0 # 2 sekund delay på scanningen.
 
-def portscan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(timeout_seconds)
-    try:
-        s.connect((host, port))
-        return True
-    except Exception:
-        return False
-    finally:
-        s.close()
-
+def portscan(port): # tilldela en funktion med (def) för portscan i detta fallet.
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socker för (ipv4 och TCP).
+    s.settimeout(timeout_seconds) # vi sätter en timeout delay för hinna scanna ordentligt.
+    try: # nu startar scriptet och vi försöker köra.
+        s.connect((host, port)) # ansluter till hosten och porten.
+        return True # om vi får anslutning
+    except Exception: # om något går fel så fångar vi up detta och förhindrar programmet att krascha helt oväntat.
+        return False # om vi inte får anslutning
+    finally: # slutet på scriptet oavsett vad.
+        s.close() # nu slutar anslutningen.
 # Steg 1: Kontroll av en enskild port (exempel port 80)
-if portscan(80):
-    print("Port 80: öppen")
-else:
-    print("Port 80: stängd")
+if portscan(80): # om portscanen 80 lyckas eller inte
+    print("Port 80: öppen") #skrivs ut i terminalen
+else: # annars om
+    print("Port 80: stängd")# skrivs ut i terminalen 
 
-print("\nSkanning av portarna 1-100 hos scanme.nmap.org/:")
+print("\nSkanning av portarna 1-100 hos scanme.nmap.org/:") # skrivs ut i terminalen
+
 # Steg 2: Skanning av portintervall
-open_ports = []
-for port in range(22, 101):
-    if portscan(port):
-        print(f"{port}: öppen")
-        open_ports.append(port)
+open_ports = [] #
+for port in range(22, 101): # portarna imellan 22 och 101 som vi vill scanna
+    if portscan(port): #
+        print(f"{port}: öppen") #
+        open_ports.append(port) #
     else:
         print(f"{port}: stängd")
-
-import socket
+# Steg 3: 
+import socket # importera socket från bibiloteket för nätverksanslutningar.
 
 def try_banner(host, port, timeout=3):
     probes = {80: b"HEAD / HTTP/1.0\r\n\r\n"}
